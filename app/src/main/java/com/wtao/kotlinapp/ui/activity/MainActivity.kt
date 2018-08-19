6527be42d9f3d60137218fe63834d382b0a27588
wangtao007@daojia.com <wangtao07@daojia.com>
2018-08-19 11:34:00 +0800

    创建各module下的目录

--- app/src/main/java/com/wtao/kotlinapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/wtao/kotlinapp/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.wtao.kotlinapp
+package com.wtao.kotlinapp.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.wtao.kotlinapp.R
 
 class MainActivity : AppCompatActivity() {
 
